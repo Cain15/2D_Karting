f5e137fafa1750b568561c320be59086936a2a3d
Cain15 <cain.hertoghs@gmail.com>
2026-02-23 21:33:14 +0100

    documentation + player property change

--- main.py
+++ main.py
@@ -5,52 +5,68 @@ from track_gen import read_track, Tile
 import time
 
 def get_tile_pos(pygame_pos):
+    """
+    Get the tile coordinates
+    :param pygame_pos: The pygame coordinates
+    :return: the tile coordinates
+    """
     x = math.floor(pygame_pos.x / TILE_SIZE)
     y = math.floor(pygame_pos.y / TILE_SIZE)
     return x,y
 
+# Screen size
 screen_width = 1600
 screen_height = 960
 
+# Initialize window
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
+# Tile variables
 TILE_SIZE = 80
 tiles_x = 20
 tiles_y = 12
 finish_tile = (16,10)
 
+# Player properties
 player_pos = pygame.Vector2(finish_tile[0]*TILE_SIZE, finish_tile[1]*TILE_SIZE + TILE_SIZE/2)
 player_angle = 90
-
 player = pygame.image.load("assets/player.png")
 player = pygame.transform.scale(player, (20,40))
-
-player_acceleration = 35
-player_deceleration = 100
+player_acceleration = 45
+player_deceleration = 108
 player_velocity = 0
+player_max_velocity = 270
 
+# Track generation
 track = read_track('track1.tr')
 corner = pygame.image.load('assets/corner.png')
 corner = pygame.transform.scale(corner, (80,80))
 straight = pygame.image.load('assets/straight.png')
 straight = pygame.transform.scale(straight, (80,80))
 
+# Progress tracking
 seen_finish = False
 prev_tile = None
 min_visited_tiles = 69
 tiles_visited = []
 amount_warnings = 0
+
+# Game state variables
 pause = 0
 message = ""
 dsq = False
 
+# Keep lap times
 lap_times = []
 
 def reset():
+    """
+    Reset the Player and Game state, pause 3 seconds
+    """
     global player_velocity, player_angle, player_pos, seen_finish, prev_tile, amount_warnings, pause, tiles_visited, start_time
     player_velocity = 0
     player_angle = 90
@@ -62,10 +78,11 @@ def reset():
     prev_tile = None
     amount_warnings = 0
 
+# Make the track
 track_surface = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
-
 for x in range(tiles_x):
     for y in range(tiles_y):
+        # Track is a list of rows. So first access y position then x position.
         tile = track[y][x]
         if tile == Tile.STRAIGHT_RIGHT:
             rotated_straight = pygame.transform.rotate(straight, 90)
@@ -90,31 +107,37 @@ for x in range(tiles_x):
             rotated_corner = pygame.transform.rotate(corner, -270)
             track_surface.blit(rotated_corner, (TILE_SIZE * x, TILE_SIZE*y))
 
+# Load font
 font = pygame.font.Font(None, 36)
 start_time = None
 
+# Game loop
 while running:
+    # Close program event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # Set background color and draw track on screen
     screen.fill((116, 179, 74))
     screen.blit(track_surface, (0,0))
 
+    # Draw the player
     rotated_surface = pygame.transform.rotate(player, player_angle)
     rotated_rect = rotated_surface.get_rect(center=player_pos)
-
     screen.blit(rotated_surface, rotated_rect)
 
+    # Check for a pause
     if not pause:
+        # Check input and act accordingly
         keys = pygame.key.get_pressed()
-        rad = math.radians(player_angle)
-        dist = player_velocity * dt
-        player_pos.y += dist * math.cos(rad)
-        player_pos.x += dist * math.sin(rad)
+        rad = math.radians(player_angle) # Angle in radians
+        dist = player_velocity * dt # The distance traveled
+        player_pos.y += dist * math.cos(rad) # Change y position
+        player_pos.x += dist * math.sin(rad) # Change x position
         if keys[pygame.K_UP]:
-            player_velocity -= player_acceleration * dt
-            player_velocity = max(-200, player_velocity)
+            player_velocity -= player_acceleration * dt # accelerate
+            player_velocity = max(-player_max_velocity, player_velocity) # max velocity is
             if not start_time:
                 start_time = pygame.time.get_ticks()
         if keys[pygame.K_DOWN]:
