b977c61c22860b6f98a09374de75963a4a96383b
Cain15 <cain.hertoghs@gmail.com>
2026-02-23 11:59:38 +0100

    pygame screen

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
 
-def main():
-    print("Hello World")
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
 
-if __name__ == "__main__":
-    main()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 200, 0))
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
